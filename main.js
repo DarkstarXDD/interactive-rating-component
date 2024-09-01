de6cfdb99be12f7a0321ccb36b0d249b263c47df
DarkstarXDD <108657985+DarkstarXDD@users.noreply.github.com>
2024-09-02 00:58:11 +0530

    feat: add custom validation

--- main.js
+++ main.js
@@ -1,7 +1,24 @@
+document.querySelector(".card__form").addEventListener("submit", (event) => {
+  event.preventDefault()
+  console.log("Submit Clicked")
+})
+
+const ratingRadioButtons = document.getElementsByName("rating")
 const resultTextElem = document.querySelector(".card__result")
+let isRadioSelected = false
+
+for (let i = 0; i < ratingRadioButtons.length; i++) {
+  if (ratingRadioButtons[i].checked) {
+    isRadioSelected = true
+  }
+}
+
+console.log(isRadioSelected)
 
 const searchParamsObj = new URLSearchParams(window.location.search)
 const rating = searchParamsObj.get("rating")
 
 const resultText = `You selected ${rating} out of 5`
-resultTextElem.innerHTML = resultText
+if (resultTextElem) {
+  resultTextElem.innerHTML = resultText
+}
